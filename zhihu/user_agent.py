e935e08b8f00b67e0da48795ea95351a82f98982
jfzhang95 <jfzhang@whu.edu.cn>
2017-08-30 22:06:54 +0800

    add user_agent

---
+++ zhihu/user_agent.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+"""
+@Author: Jeff Zhang
+@Time:   17-8-30 下午10:05
+@File:   user_agent.py
+"""
+
+
+user_agents = [
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+]
